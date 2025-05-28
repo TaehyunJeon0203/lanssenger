4b8e75f0927325da6e3f61d4e51be20ef8495dda
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-05-28 12:04:06 +0900

    Build: windows 빌드 스크립트 수정

--- scripts/build_client.bat
+++ scripts/build_client.bat
@@ -4,9 +4,7 @@ mkdir build
 
 REM CMake로 빌드
 cd build
-cmake .. -DBOOST_ROOT="C:/Program Files/boost" ^
-    -DBoost_INCLUDE_DIR="C:/Program Files/boost/include" ^
-    -DBoost_LIBRARY_DIR="C:/Program Files/boost/lib"
+cmake .. -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build . --target client client_gui
 
 echo 클라이언트 빌드가 완료되었습니다.
