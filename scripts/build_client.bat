e818a72c2620fec0fa63644fdd0cbbecf2453ab7
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-05-28 10:01:46 +0900

    Build: windows용 client 빌드 스크립트 생성

---
+++ scripts/build_client.bat
@@ -0,0 +1,12 @@
+@echo off
+REM 빌드 디렉토리 생성
+mkdir build
+
+REM CMake로 빌드
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build . --target client client_gui
+
+echo 클라이언트 빌드가 완료되었습니다.
+echo 콘솔 버전 실행: .\build\client.exe
+echo GUI 버전 실행: .\build\client_gui.exe 
