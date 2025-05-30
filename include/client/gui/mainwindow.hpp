5d2685217ed7e2e1ad9bf157542c2489ba398aa9
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-05-30 09:28:23 +0900

    delete

--- include/client/gui/mainwindow.hpp
+++ include/client/gui/mainwindow.hpp
@@ -3,9 +3,7 @@
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }  // Qt Designer 클래스 이름에 맞게 수정
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
--- src/client/gui/mainwindow.cpp
+++ src/client/gui/mainwindow.cpp
@@ -1,24 +1,26 @@
-#include "mainwindow.hpp"
+#include "client/gui/mainwindow.hpp"
 #include "ui_mainwindow.h"  // CMake가 자동 생성한 ui 헤더
 
 #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    connect(ui->pushButton_4, &QPushButton::clicked, this, &MainWindow::sendMessage);
+    connect(ui->sendButton, &QPushButton::clicked, this, &MainWindow::sendMessage);
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow()
+{
     delete ui;
 }
 
-void MainWindow::sendMessage() {
-    QString message = ui->myChat->toPlainText().trimmed();
+void MainWindow::sendMessage()
+{
+    QString message = ui->messageInput->toPlainText();
     if (!message.isEmpty()) {
-        ui->currentChat->append(message);
-        ui->myChat->clear();
+        ui->chatDisplay->append("나: " + message);
+        ui->messageInput->clear();
     }
 }
