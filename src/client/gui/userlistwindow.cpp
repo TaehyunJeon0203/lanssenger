2a48c72b597e1026e937ae11d4c25b5ff2f6870b
youbyeongyeon <youby020919@gmail.com>
2025-06-05 15:05:29 +0900

    Feat: UserlistMenu 닫기구현

--- src/client/gui/userlistwindow.cpp
+++ src/client/gui/userlistwindow.cpp
@@ -1,15 +1,29 @@
 #include "client/gui/userlistwindow.hpp"
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QPushButton> 
 
 UserListWindow::UserListWindow(QWidget *parent) : QWidget(parent) {
     setWindowTitle("유저 목록");
     resize(300, 400);
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    userListWidget = new QListWidget(this);
-    layout->addWidget(userListWidget);
 
-    // 닫기 버튼 제거 (우측 상단 X 버튼만 사용)
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
+    // 상단에 닫기 버튼 배치
+    QHBoxLayout *topLayout = new QHBoxLayout();
+    topLayout->addStretch(); // 오른쪽 정렬
+    QPushButton *closeButton = new QPushButton("✕", this);
+    closeButton->setFixedSize(24, 24);
+    closeButton->setToolTip("닫기");
+    topLayout->addWidget(closeButton);
+    mainLayout->addLayout(topLayout);
+
+    userListWidget = new QListWidget(this);
+    mainLayout->addWidget(userListWidget);
+
+    connect(closeButton, &QPushButton::clicked, this, &QWidget::close);
+
+    // 윈도우 X 버튼도 사용 가능
     setWindowFlags(windowFlags() | Qt::WindowCloseButtonHint);
 }
 
