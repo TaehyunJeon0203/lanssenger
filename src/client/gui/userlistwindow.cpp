036e91e7120e74622c9b5fba4a8f906424fd654b
youbyeongyeon <youby020919@gmail.com>
2025-06-05 14:59:24 +0900

    Feat: UserlistMenu 닫기구현

--- src/client/gui/userlistwindow.cpp
+++ src/client/gui/userlistwindow.cpp
@@ -12,4 +12,24 @@ UserListWindow::UserListWindow(QWidget *parent) : QWidget(parent) {
 void UserListWindow::updateUserList(const QStringList& users) {
     userListWidget->clear();
     userListWidget->addItems(users);
+}#include "client/gui/userlistwindow.hpp"
+#include <QVBoxLayout>
+#include <QPushButton> 
+
+UserListWindow::UserListWindow(QWidget *parent) : QWidget(parent) {
+    setWindowTitle("유저 목록");
+    resize(300, 400);
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    userListWidget = new QListWidget(this);
+    layout->addWidget(userListWidget);
+
+
+    QPushButton *closeButton = new QPushButton("닫기", this);
+    layout->addWidget(closeButton);
+    connect(closeButton, &QPushButton::clicked, this, &QWidget::close);
+}
+
+void UserListWindow::updateUserList(const QStringList& users) {
+    userListWidget->clear();
+    userListWidget->addItems(users);
 }
