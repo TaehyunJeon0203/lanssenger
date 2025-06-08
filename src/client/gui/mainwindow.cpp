3fc55daf13ff1c08263617494490fd514bfe9f5c
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-06-08 17:34:16 +0900

    Fix: 채팅 표시창 입력되던 오류 수정

--- src/client/gui/mainwindow.cpp
+++ src/client/gui/mainwindow.cpp
@@ -16,6 +16,7 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    ui->chatDisplay->setReadOnly(true);
     setupMenuBar();
     setupConnections();
     connectToServer();
